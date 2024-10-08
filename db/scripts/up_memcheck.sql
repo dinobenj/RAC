414777f7a75a82965a07f3a3b431b38643e6a4aa
dinobenj <dino.benj@gmail.com>
2024-10-08 17:06:11 -0400

    added more tables

--- db/scripts/up_memcheck.sql
+++ db/scripts/up_memcheck.sql
@@ -1,8 +1,10 @@
 CREATE TABLE memcheck (
-    session_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    session_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     last_modified TIMESTAMP NOT NULL,
     memcheck INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (session_id) REFERENCES sessions (session_id)
 );
 
---
+++ db/scripts/up_sessions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sessions (
+    session_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    memcheck INTEGER NOT NULL,
+    dll_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (dll_id) REFERENCES dlls (id)
+);
+--Will add more columns to this table later as more features are added
