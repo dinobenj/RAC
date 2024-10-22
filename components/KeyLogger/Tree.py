501eecfb5034c001816e70157c87c48ca80c1c20
Devin <dross9431@gmail.com>
2024-10-22 17:30:03 -0400

    Simple tree logging working

--- components/KeyLogger/Tree.py
+++ components/KeyLogger/Tree.py
@@ -5,13 +5,15 @@ class TreeNode:
   def __init__(self, name: str):
     self.name = name
     self.children: list[TreeNode] = []
+    self.strength = 1
 
   def add_child(self, child_node: 'TreeNode'):
     self.children.append(child_node)
 
   def __repr__(self):
-    # return self.print_tree_by_children()[:-1]
-    return self.print_tree_by_level()[:-1]
+    return self.print_tree_by_children()[:-1]
+    # return str(self.print_tree_by_level()[:-1])
+    # return str(self.print_tree_by_level())
   
   def print_tree_by_children(self, level=0):
     # print(level, type(level))
@@ -20,5 +22,15 @@ class TreeNode:
       ret += child.print_tree_by_children(level + 1)
     return ret
 
+  # def print_tree_by_level(self, level=0):
+  #   # [[[]]]
+  #   # List of depths, depths are lists of groups of children of specific parent
 
+  #   flock_of_children = {}
+
+  #   flock_of_children[level] = []
+  #   for child in self.children:
+  #     flock_of_children[level].append(child.print_tree_by_level(level+1))
+    
+  #   return flock_of_children
 
--- components/KeyLogger/log.py
+++ components/KeyLogger/log.py
@@ -1,6 +1,10 @@
+from Tree import TreeNode
 
 cluster_start = -1
 in_cluster = True
+
+trees: dict[chr, TreeNode] = {}
+current_node: TreeNode = None
 
 def cluster(char, time):
   f = open("Logs/testLog.txt", "a")
@@ -25,10 +29,33 @@ def cluster(char, time):
   print(f"{char} at {time}")
   f.close()
 
+def tree(char):
+  global trees, current_node
+  print(trees.keys())
+  if char in trees.keys():
+    print("Tree exists. Checking children")
+    node = trees[char]
 
+    found = 0
+    for child in node.children:
+      if char == child.name:
+        print("Found child. Increasing strength and breaking")
+        child.strength += 1
+        current_node = child
+        found = 1
+        break
+    if not found:
+      print("Child does not exist. Creating child")
+      node.add_child(TreeNode(char))
+
+  else:
+    print("Tree does not exist. Creating tree")
+    trees[char] = TreeNode(char)
 
 def log(char, time):
-  cluster(char, time)
+  # cluster(char, time)
+  # print(char, type(char))
+  tree(char)
   
 
 
