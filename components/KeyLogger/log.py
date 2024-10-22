e91ba967748d52588876ed9fd8ec1be9b3e2a4f6
Devin <dross9431@gmail.com>
2024-10-22 14:03:51 -0400

    Simple Cluster Logging

--- components/KeyLogger/log.py
+++ components/KeyLogger/log.py
@@ -1,4 +1,28 @@
 
+cluster_start = -1
+in_cluster = True
 
+def log(char, time):
+  f = open("Logs/testLog.txt", "a")
+  global cluster_start
+  # print(time-cluster_start)
+
+  if time > 0.2:
+    f.write("\n> ")
+  print(char)
+  if len(char) == 1:
+    f.write(char)
+
+  # if time < 0.2:
+  #   if len(char) == 1:
+  #     f.write(char)
+  # else:
+  #   print("NEW CLUSTER STARTS")
+  #   f.write("\n> ")
+  #   if len(char) == 1:
+  #     f.write(char)
+  #   cluster_start = time
+  print(f"{char} at {time}")
+  f.close()
 
 
--- components/KeyLogger/read_key_and_mouse.py
+++ components/KeyLogger/read_key_and_mouse.py
@@ -1,7 +1,10 @@
 from pynput import keyboard, mouse
+from time import time
 # from key_to_picture import addPixel, loadImage
+from log import log
 
 # im = -1
+start = time()
 
 # Mouse functions
 
@@ -12,14 +15,19 @@ def mouse_move(x, y):
 # Keyboard functions
 
 def key_press(key):
+  global start
   if key == keyboard.Key.esc:
-    mouse_listener.stop()
+    # mouse_listener.stop()
     return False
   try:
     key_name = key.char # type str
   except:
     key_name = key.name
-  print('Key Pressed: ' + key_name)
+  # print('Key Pressed: ' + key_name)
+  old = start
+  start = time()
+  log(key_name, start - old)
+
 
 
 # def key_press(key):
@@ -35,14 +43,16 @@ def key_press(key):
 # Running the listener
 
 if __name__ == "__main__":
+  f = open("Logs/testLog.txt", "w")
+  f.close()
   # im = loadImage("Pictures/pica.png")
 
   keyboard_listener = keyboard.Listener(on_press=key_press)
-  mouse_listener = mouse.Listener(on_move=mouse_move)
+  # mouse_listener = mouse.Listener(on_move=mouse_move)
 
   keyboard_listener.start()
-  # keyboard_listener.join()
+  keyboard_listener.join()
 
-  mouse_listener.start()
-  mouse_listener.join()
+  # mouse_listener.start()
+  # mouse_listener.join()
 
