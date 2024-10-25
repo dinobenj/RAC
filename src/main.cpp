bd3d2f15f65d9014b446db24585357ae2ff3f5ed
dinobenj <dino.benj@gmail.com>
2024-10-25 17:29:15 -0400

    More stuff for compiling

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,21 @@
 
 namespace fs = std::filesystem;
 
+
+void compileCppFiles(const std::vector<fs::path>& cppFiles, const fs::path& outputDir) {
+    for (const auto& file : cppFiles) {
+        std::string outputName = outputDir / file.stem();
+        std::string command = "g++" + file.string() + "-o " + outputName + " " + file.string();
+
+        std::cout << "Compiling: " << file << std::endl;
+        if (std::system(command.c_str()) != 0) {
+            std::cerr << "Failed to compile: " << file << std::endl;
+        } else {
+            std::cout << "Compiled successfully: " << outputName << std::endl;
+        }
+    }
+}
+
 void findFiles(const fs::path& directory, std::vector<fs::path>& cppFiles) {
     for (const auto& entry : fs::recursive_directory_iterator(directory)) {
         if (entry.is_regular_file() && entry.path().extension() == ".cpp") {
@@ -21,6 +36,7 @@ int main() {
     for (const auto& file : files) {
         std::cout << file << std::endl;
     }
+    compileCppFiles(files, "output");
 
     return 0;
 }
