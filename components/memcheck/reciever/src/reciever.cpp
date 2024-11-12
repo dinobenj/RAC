84e11da231295e480cf8def6409d0b4e88bae898
dinobenj <dino.benj@gmail.com>
2024-11-12 17:32:45 -0500

    deez

---
+++ components/memcheck/reciever/src/reciever.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <cstdlib>
+#include "checksum_packet.h"
+
+
+
+
+
+
+
+
+int main() {
+    
+
+    return 0;
+}
---
+++ components/memcheck/reciever/src/reciever.h
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <cstdlib>
+#include <mutex>
+
+class reciever {
+    public:
+        reciever();
+        ~reciever();
+        void start();
+        void stop();
+        void receive();
+
+    private:
+        int socket_fd;
+        std::vector<uint8_t> buffer;
+        std::mutex mtx;
+        bool running;
+
+        void init_socket();
+        void close_socket();
+};
